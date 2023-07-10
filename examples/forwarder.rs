a29a61f90f433c94a4f0d9a0f9802f54f2f4cd25
C-o-r-E <can.of.tuna@gmail.com>
2023-07-10 17:53:13 -0400

    Attempt to better conform with codebase

--- examples/forwarder.rs
+++ examples/forwarder.rs
@@ -1,35 +1,34 @@
 //! Forwarder example based on https://github.com/C-o-r-E/nng_rs_pubsub_forwarder
 //!
 //! This example shows how to use raw sockets to set up a forwarder or proxy for pub/sub.
-//! 
+//!
 //! An example setup for running this example would involve the following:
-//! 
-//!  - Run this example binary (in the background or a terminal, etc)
+//!
+//!  - Run this example binary (in the background or a terminal, etc) `forwarder tcp://localhost:3327 tcp://localhost:3328`
 //!  - In a new terminal, run `nngcat --sub --dial "tcp://localhost:3328" --quoted`
 //!  - In a second terminal, run `nngcat --sub --dial "tcp://localhost:3328" --quoted`
 //!  - In a third terminal, run `for n in $(seq 0 99); do nngcat --pub --dial "tcp://localhost:3327" --data "$n"; done`
 
-use nng::{
-    RawSocket, Protocol, forwarder,
-};
+use nng::{forwarder, Error, Protocol, RawSocket};
+use std::{env, process};
 
-const FRONT_URL: &str = "tcp://localhost:3327";
-const BACK_URL: &str = "tcp://localhost:3328";
+fn main() -> Result<(), Error> {
+    let args: Vec<_> = env::args().collect();
 
-fn main() {
-    let front_end: RawSocket = RawSocket::new(Protocol::Sub0).unwrap();
-    let back_end:  RawSocket = RawSocket::new(Protocol::Pub0).unwrap();
+    if args.len() < 3 {
+        println!("Usage: {} <FRONT_END_URL> <BACK_END_URL>", &args[0]);
+        process::exit(1);
+    }
 
-    let ret = front_end.socket.listen(FRONT_URL);
-    if let Err(x) = ret { panic!("failed to listen on front end: {}", x) }
+    do_forwarding(&args[1], &args[2])
+}
 
-    let ret = back_end.socket.listen(BACK_URL);
-    if let Err(x) = ret { panic!("failed to listen on back end: {}", x) }
+fn do_forwarding(front_url: &str, back_url: &str) -> Result<(), Error> {
+    let front_end: RawSocket = RawSocket::new(Protocol::Sub0)?;
+    let back_end: RawSocket = RawSocket::new(Protocol::Pub0)?;
 
-    let ret = forwarder(
-        front_end,
-        back_end
-    );
+    front_end.socket.listen(front_url)?;
+    back_end.socket.listen(back_url)?;
 
-    if let Err(fw_err) = ret { println!("Forwarder exited with err: {}", fw_err) }
+    forwarder(front_end, back_end)
 }
